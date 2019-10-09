8960e804bd93c340bda9892205d852a3cf36dc6a
William Embry <wembry@student.cscc.edu>
2019-10-09 13:58:48 -0400

    Initial commit

---
+++ src/edu/cscc/SquareAndCountTest.java
@@ -0,0 +1,23 @@
+package edu.cscc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SquareAndCountTest {
+
+    @Test
+    public void square() {
+        SquareAndCount testerObj = new SquareAndCount();
+        int result = testerObj.square(19);
+        Assert.assertEquals(result,361);
+    }
+
+    @Test
+    public void countA() {
+        SquareAndCount testerObj = new SquareAndCount();
+        int result = testerObj.countA("abaracadabara");
+        Assert.assertEquals(result, 7);
+    }
+}
